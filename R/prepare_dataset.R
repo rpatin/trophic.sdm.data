d65eafa4d4d0e3b8d08cfa1cc3fd315181de046e
rpatin <rpatin@github.com>
2023-06-12 16:08:25 +0200

    updated prepare_dataset

--- R/prepare_dataset.R
+++ R/prepare_dataset.R
@@ -18,6 +18,9 @@ prepare_dataset <- function(checklist,
                             data.mask,
                             project.name,
                             nb.cpu,
+                            overwrite.occurrence = FALSE,
+                            overwrite.trophic = FALSE,
+                            subsample.assemble,
                             ...){
   
   cli_h1("Prepare trophic dataset data")
@@ -31,7 +34,10 @@ prepare_dataset <- function(checklist,
     folder.iucn = folder.iucn,
     data.mask = data.mask, 
     project.name = project.name,
-    nb.cpu = nb.cpu)
+    nb.cpu = nb.cpu,
+    overwrite.occurrence = overwrite.occurrence,
+    overwrite.trophic = overwrite.trophic,
+    subsample.assemble = subsample.assemble)
   for (argi in names(args)) { assign(x = argi, value = args[[argi]]) }
   
   ## initialize folder -----------------------------------------------------
@@ -59,201 +65,34 @@ prepare_dataset <- function(checklist,
   link_logfile_step2 <- paste0(project.name,"/log/",logfile_step2)
   if (file.exists(link_logfile_step2)) file.remove(link_logfile_step2)
   
-  # browser()
-  this.species = names(listcode)[1]
-  # this.species = names(listcode)[456]
-  # this.species = names(listcode)[510]
-  
   cli_progress_done()
   ## Step 1 - Buffer IUCN ranges + Generate Species Occurrence dataset ---------
   cli_h2("Step 1 - Individual species occurrence dataset")
+  this.species = names(listcode)[1]
   species.occurrence <- foreach(this.species = names(listcode)) %dopar% {
-    cli_progress_step(this.species)
     this.code <- listcode[this.species]
-    this.output <- load_gbif_data(species.name = this.species,
-                                  folder.gbif = param.gbif@folder.gbif, 
-                                  filter.atlas = param.gbif@filter.atlas)
-    this.summary <- "failed"
-    this.status <- "failed"
-    this.iucn <- "failed"
-    this.occurrence <- "failed"
-    if (!inherits(this.output,"data.frame")) {
-      # no gbif data
-      this.reason <- this.output
-    } else if (nrow(this.output) < param.gbif@min.gbif) {
-      # less than min.gbif data
-      this.reason <- paste0("less than ", param.gbif@min.gbif, " occurrence")
-    } else if (first(this.output$distance_to_iucn) != "No distribution found" &&
-               all(this.output$distance_to_iucn > 
-                   param.gbif@buffer.config[[
-                     param.gbif@species.buffer[[this.species]]]]*1000
-               )) {
-      this.reason <- paste0("no occurrences within IUCN range")
+    if (param.gbif@use.gbif) {
+      gbif.output <- extract_gbif(this.species,
+                                  this.code,
+                                  param.gbif, 
+                                  occurrence.dir = occurrence.dir,
+                                  project.name = project.name,
+                                  logfile = logfile_step1,
+                                  overwrite = overwrite.occurrence) 
+      return(gbif.output)
     } else {
-      # more than min.gbif data
-      this.occurrence <- paste0(occurrence.dir, this.code, ".csv")
-      iucn.try <- try({
-        this.iucn <-
-          locate_iucn_distribution(species.code = listcode[this.species],
-                                   folder.iucn = param.gbif@folder.iucn.buffer,
-                                   filetype = '.tif')
-        
-        if (first(this.output$distance_to_iucn) == "No distribution found" ||
-            is.null(this.iucn)) {
-          this.output <- 
-            mutate(this.output, inside_iucn = TRUE)
-          this.iucn <- "none"
-        } else {
-          this.buffer <- 
-            param.gbif@buffer.config[[
-              param.gbif@species.buffer[[this.species]]]]*1000
-          
-          # buffer presences
-          this.output <- 
-            mutate(this.output,
-                   distance_to_iucn = as.numeric(distance_to_iucn),
-                   inside_iucn = distance_to_iucn <= this.buffer)
-          
-          # get IUCN distribution
-          this.iucn.rast <- rast(this.iucn)
-        }
-      })
-      if (inherits(iucn.try, "try-error")) {
-        this.reason <- "Could not retrieve IUCN information"
-        this.iucn <- "failed"
-      } else {
-        certain.try <- try({
-          ### Rule 1 filter out gbif data outside IUCN ------------------
-          # 
-          # transform to SpatVector
-          # project to ETRS1989
-          this.output.vect <- 
-            this.output %>% 
-            filter(inside_iucn) %>%  # Rule 1
-            vect(geom = c("X","Y"), crs = crs("EPSG:4326"))
-          this.output.etrs <- project(this.output.vect, crs("EPSG:3035"))
-          
-          # rasterize occurrence to 0/1
-          this.occurrence.rast <- 
-            this.output.etrs %>% 
-            rasterize(data.mask, background = 0) %>% 
-            mask(mask = data.mask, maskvalue = NA)
-          
-          # transform into data.frame
-          this.occurrence.df <- 
-            this.occurrence.rast %>% 
-            as.data.frame(cells = TRUE, na.rm = TRUE, xy = TRUE) %>% 
-            rename(presence = mask.grid)
-          
-          # add inside/outside IUCN range
-          if (this.iucn == "none") {
-            this.occurrence.df$inside_iucn <- TRUE
-          } else {
-            this.iucn.df <- 
-              this.iucn.rast %>% 
-              as.data.frame(cells = TRUE, na.rm = TRUE, xy = TRUE) %>% 
-              rename(inside_iucn = layer) %>% 
-              mutate(inside_iucn = (inside_iucn == 1))
-            this.occurrence.df <- 
-              left_join(this.occurrence.df, this.iucn.df,
-                        by = c("cell","x","y"))
-          }
-          
-          # transform absences into certain/uncertain
-          this.occurrence.df$status <- NA
-          
-          ### Rule 2: presence inside IUCN are certain ------------------------
-          this.occurrence.df$status[
-            which(this.occurrence.df$presence == 1) 
-          ] <- "certain"
-          
-          ### Rule 3: absences outside IUCN are certain ----------------------
-          this.occurrence.df$status[
-            which(this.occurrence.df$presence == 0 &
-                    !this.occurrence.df$inside_iucn) 
-          ] <- "certain"
-          
-          ### Rule 4: absences within IUCN needs a check against sampling effort -------
-          index.uncertain <- which(this.occurrence.df$presence == 0 &
-                                     this.occurrence.df$inside_iucn) 
-          if(length(index.uncertain) > 0){
-            this.effort.layer <- sampling.effort@species.layer[[this.species]]
-            this.effort.rast <- rast(sampling.effort@data)[[this.effort.layer]]
-            this.quantile <-
-              param.gbif@quantile.absence.certain
-            # extract threshold to caracterize absence certainty
-            # i.e. quantile of sampling effort in presence cell
-            this.presence.vect <- 
-              this.occurrence.df %>% 
-              filter(presence == 1) %>% 
-              vect(geom = c("x","y"), crs = crs("EPSG:3035"))
-            this.effort <- extract(this.effort.rast,
-                                   this.presence.vect)[, this.effort.layer]
-            this.threshold <- quantile(this.effort, probs = this.quantile, type = 3)
-            this.uncertain.vect <- 
-              this.occurrence.df[index.uncertain,] %>% 
-              vect(geom = c("x","y"), crs = crs("EPSG:3035"))
-            this.effort.uncertain <- extract(this.effort.rast,
-                                             this.uncertain.vect)[, this.effort.layer]
-            this.occurrence.df$status[index.uncertain] <- 
-              ifelse(this.effort.uncertain >= this.threshold, "certain","uncertain")
-          }
-          this.occurrence <- paste0(occurrence.dir,this.code,".csv.gz")
-          this.occurrence.df$SpeciesName <- this.species
-          this.occurrence.df$Code <- this.code
-          fwrite(this.occurrence.df, file = this.occurrence)
-          
-          ### Summary --------------------------
-          n.presences <- length(which(this.occurrence.df$presence == 1))
-          n.absences <- length(which(this.occurrence.df$presence == 0))
-          n.absences.outside <- 
-            length(which(this.occurrence.df$presence == 0 &
-                           !this.occurrence.df$inside_iucn))
-          n.absences.inside.certain <-
-            length(which(this.occurrence.df$presence == 0 &
-                           this.occurrence.df$inside_iucn & 
-                           this.occurrence.df$status == "certain"))
-          n.absences.inside.uncertain <-
-            length(which(this.occurrence.df$presence == 0 &
-                           this.occurrence.df$inside_iucn & 
-                           this.occurrence.df$status == "uncertain"))
-          this.summary <- 
-            data.frame("SpeciesName" = this.species,
-                       "Code" = this.code,
-                       "presence" = n.presences,
-                       "absence" = n.absences,
-                       "absence.outside" = n.absences.outside,
-                       "absence.inside.certain" = n.absences.inside.certain,
-                       "absence.inside.uncertain" = n.absences.inside.uncertain,
-                       "threshold.certain" = this.threshold)
-        })
-        
-        if (inherits(certain.try, "try-error")){
-          this.reason <- "Error in rasterization/occurrence assessment"
-        } else {
-          this.status <- "success"
-          this.reason <- "success"
-        }
-      }
+      iucn.output <- extract_iucn(this.species, 
+                                  this.code, 
+                                  folder.iucn = param@folder.iucn, 
+                                  occurrence.dir = occurrence.dir,
+                                  project.name = project.name,
+                                  logfile = logfile_step1,
+                                  overwrite = overwrite.occurrence)
+      return(iucn.output)
     }
-    .write_logfile(
-      out.log = paste0("Species: ", this.species, 
-                       ". Status: ",this.status,
-                       ". Reason: ", this.reason),
-      logfile = logfile_step1,
-      project.name = project.name,
-      open = "a",
-      silent = TRUE)
-    cli_progress_done()
-    return(list("SpeciesName" = this.species,
-                "Code" = this.code,
-                "status" = this.status,
-                "reason" = this.reason,
-                "IUCN.file" = this.iucn,
-                "occurrence.file" = this.occurrence,
-                "occurrence.summary" = this.summary))
   } # end foreach Step 1
-  
+  # browser()
+  ## Summary Step 1 - Occurrence dataset ---------
   occurrence.summary <- lapply(species.occurrence, function(x){
     if (inherits(x$occurrence.summary, "data.frame")) {
       return(x$occurrence.summary)
@@ -276,6 +115,20 @@ prepare_dataset <- function(checklist,
             })) 
   if (is.null(filtered.species)) filtered.species <- character()
   
+  fresh.occurrence <- 
+    do.call('c', 
+            lapply(species.occurrence, function(x){
+              if (x$status == "success") {
+                y <- x$fresh
+                names(y) <- x$SpeciesName  
+                return(y)
+              } else {
+                return(NULL)
+              }
+            })) 
+  # browser()
+  if (is.null(filtered.species)) filtered.species <- character()
+  
   file.occurrence.link <- 
     do.call('c', 
             lapply(species.occurrence, function(x){
@@ -288,15 +141,28 @@ prepare_dataset <- function(checklist,
               }
             }))
   
+  IUCN.link <- 
+    do.call('c', 
+            lapply(species.occurrence, function(x){
+              if (x$status == "failed" || x$IUCN.file == "failed") {
+                return(NULL)
+              } else {
+                y <- x$IUCN.file
+                names(y) <- x$SpeciesName  
+                return(y)
+              }
+            }))
+  # browser()
   ## Step 2 - filter presence/absence based on prey community ---------
   cli_h2("Step 2 - Assemble prey dataset")
+  # browser()
   
   listname.full <- checklist$SpeciesName
   names(listname.full) <- checklist$Code
   
   listcode <- occurrence.summary$Code
   names(listcode) <- occurrence.summary$SpeciesName
-  
+  # browser()
   # this.species = names(listcode)[8]
   # browser()
   species.trophic.list <- foreach(this.species = names(listcode)) %dopar% {
@@ -304,58 +170,19 @@ prepare_dataset <- function(checklist,
     this.code <- listcode[this.species]
     
     step2.try <- try({
-      ### filter metaweb ---------------------
-      this.metaweb <- 
-        metaweb %>% 
-        filter(Pred_Code_new == this.code,
-               Prey_Code_new %in% listcode,
-               Co_occur >= 1,
-               Score_DD == 2,
-               Stage == "adults")
-      
-      this.occurrence <- fread(file.occurrence.link[this.species])
-      this.trophic.file <- paste0(raw.dir, this.code,".csv.gz")
-      
-      if (nrow(this.metaweb) == 0) {
-        ### no prey ---------------------
-        this.trophic <- 
-          this.occurrence %>% 
-          filter(status == "certain") %>% 
-          select(-status) %>% 
-          mutate(subsample = TRUE)
-        fwrite(this.trophic, this.trophic.file)
-      } else {
-        ### with prey ---------------------
-        all.prey <- this.metaweb$Prey_Name
-        # this.prey <- first(this.metaweb$Prey_Name)
-        this.prey.df <- foreach(this.prey = all.prey, .combine = 'rbind') %do% {
-          fread(file.occurrence.link[this.prey])
-        }
-        this.prey.certain <- 
-          this.prey.df %>% 
-          group_by(cell,x,y) %>% 
-          summarize(percent_certain = length(which(status == "certain"))/n(),
-                    .groups = 'drop') %>% 
-          filter(percent_certain >= param.gbif@prop.prey.certain)
-        
-        this.prey.pivot <- 
-          this.prey.df %>% 
-          semi_join(this.prey.certain, by = c("cell","x","y")) %>% 
-          mutate(presence = ifelse(status == "certain", presence,
-                                   param.gbif@uncertain.value)) %>% 
-          select(-SpeciesName, -inside_iucn, -status) %>% 
-          pivot_wider(names_from = Code, values_from = presence)
-        
-        rm(this.prey.df); gc()
-        
-        this.trophic <- 
-          right_join(this.occurrence, this.prey.pivot, 
-                     by = c("cell","x","y")) %>% 
-          filter(status == "certain") %>% 
-          select(-status) %>% 
-          mutate(subsample = TRUE)
-        fwrite(this.trophic, this.trophic.file)
-      }
+      tmp <- assemble_trophic(this.species,
+                              listcode,
+                              file.occurrence.link,
+                              raw.dir = raw.dir,
+                              param,
+                              overwrite = overwrite.trophic,
+                              fresh.occurrence = fresh.occurrence,
+                              subsample.assemble =  subsample.assemble,
+                              IUCN.link = IUCN.link)
+      this.trophic <- tmp$this.trophic
+      this.trophic.file <- tmp$this.trophic.file
+      this.metaweb <- tmp$this.metaweb
+      rm(tmp); gc();
     })
     if (inherits(step2.try, "try-error")) {
       cli_process_failed()
@@ -367,7 +194,8 @@ prepare_dataset <- function(checklist,
         open = "a",
         silent = TRUE)
     } 
-    
+    # browser()
+
     # if (this.species == "Accipiter nisus") browser()
     ### Summary --------------------------------------------------------------
     summary.try <- try({
@@ -378,7 +206,7 @@ prepare_dataset <- function(checklist,
       prey.summary <-  get_prey_summary(this.trophic)
       # filtered prey
       this.metaweb.full <- 
-        metaweb %>% 
+        param@metaweb.raw %>% 
         filter(Pred_Code_new == this.code,
                Co_occur >= 1,
                Score_DD == 2,
@@ -395,8 +223,8 @@ prepare_dataset <- function(checklist,
       filtered.prey.list <- list(filtered.prey)
       names(filtered.prey.list) <- this.species
     })
-    # if (this.species == "Accipiter nisus") browser()
-    
+
+
     if (inherits(summary.try, "try-error")) {
       .write_logfile(
         out.log = paste0("Species: ", this.species, 
@@ -430,7 +258,8 @@ prepare_dataset <- function(checklist,
                   "filtered.prey" = filtered.prey.list))
     }
   }
-  
+  # browser()
+
   ## Format Output -------------------------------------------------------------
   summary.predator <- rbindlist(
     lapply(species.trophic.list, function(x) x$pred.summary)
@@ -491,6 +320,7 @@ prepare_dataset <- function(checklist,
   
   filtered.prey <- sapply(species.trophic.list, function(x) x$filtered.prey)
   output <- new('trophic_dataset')
+  # browser()
   
   # fill output@summary
   output@summary@occurrence <- occurrence.summary
@@ -498,7 +328,7 @@ prepare_dataset <- function(checklist,
   output@summary@prey <- summary.prey
   output@summary@filtered <- summary.filter
   output@summary@protected <- data.frame()
-  output@summary@patrimonial <- data.frame()
+  # output@summary@patrimonial <- data.frame()
   
   output@summary.raw <- output@summary
   # fill output@files
@@ -519,9 +349,11 @@ prepare_dataset <- function(checklist,
   output@project.name <- project.name
   output@data.mask <- wrap(data.mask)
   saveRDS(output, file = paste0(project.name,"/",project.name,".trophic_dataset.rds"))
+  validObject(output)
   
   ## Subsample & Filter output ------------------------------------------------
   cli_h1("Subsampling dataset")
+  # browser()
   
   cli_progress_step("Subsample dataset")
   output <- subsample(output)
@@ -541,7 +373,10 @@ prepare_dataset <- function(checklist,
                                         folder.iucn,
                                         data.mask, 
                                         project.name,
-                                        nb.cpu){
+                                        nb.cpu,
+                                        overwrite.occurrence,
+                                        overwrite.trophic,
+                                        subsample.assemble){
   
   
   #### checklist -------------------------------------------------------------
@@ -585,6 +420,17 @@ prepare_dataset <- function(checklist,
     .fun_testIfInherits(param.subsampling, "param_subsampling")
   }
   
+  #### overwrite -------------------------------------------
+  stopifnot(is.logical(overwrite.occurrence))
+  stopifnot(is.logical(overwrite.trophic))
+  
+  #### subsample.assemble -------------------------------------------
+  if (missing(subsample.assemble)) {
+    subsample.assemble <- ifelse(use.gbif, FALSE, TRUE)
+  } else {
+    stopifnot(is.logical(subsample.assemble))
+  }
+  
   return(list(
     "checklist" = checklist, 
     "metaweb" = metaweb,
@@ -594,6 +440,9 @@ prepare_dataset <- function(checklist,
     "folder.iucn" = folder.iucn,
     "data.mask" = data.mask, 
     "project.name" = project.name,
-    "nb.cpu" = nb.cpu))
+    "nb.cpu" = nb.cpu,
+    "overwrite.occurrence" = overwrite.occurrence,
+    "overwrite.trophic" = overwrite.trophic,
+    "subsample.assemble" = subsample.assemble))
 }
 
