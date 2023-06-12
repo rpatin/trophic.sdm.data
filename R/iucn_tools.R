a8f59293e7a96b675874aed47f43380e3379c71b
rpatin <rpatin@github.com>
2023-06-12 16:07:46 +0200

    update to IUCN tools

--- R/iucn_tools.R
+++ R/iucn_tools.R
@@ -21,58 +21,34 @@
 ##'   Family, Species) and associated distance that will be used to buffer IUCN
 ##'   range. If several taxonomic groups are valid for a given species, the
 ##'   lowest one will be prioritized. Buffer distances are given in km.
-##' @param do.buffer a \code{boolean}. TRUE if buffered distribution 
-##' needs to be calculated
-##' @param do.raster a \code{boolean}. TRUE if raw distribution needs to
-##' be calculated
 ##' @importFrom terra focal rast vect buffer aggregate rasterize
 
 rasterize_iucn <- function(checklist, 
                            folder.iucn,
-                           list.iucn,
                            data.mask,
                            project.name,
-                           buffer.config,
-                           nb.cpu = 1,
-                           do.raster = TRUE,
-                           do.buffer = TRUE){
+                           nb.cpu = 1){
   
-  cli_h1("Rasterize and Buffer IUCN range maps")
+  cli_h1("Rasterize IUCN range maps")
   cli_progress_step("Argument check")
   args <- .rasterize_iucn.check.args(checklist = checklist, 
                                      folder.iucn = folder.iucn,
-                                     list.iucn = list.iucn,
                                      data.mask = data.mask,
                                      project.name = project.name,
-                                     buffer.config = buffer.config,
-                                     nb.cpu = nb.cpu,
-                                     do.raster = do.raster,
-                                     do.buffer = do.buffer)
+                                     nb.cpu = nb.cpu)
   for (argi in names(args)) { assign(x = argi, value = args[[argi]]) }
   
   has.cluster <- .register_cluster(nb.cpu = nb.cpu)
   
-  # browser()
-  
-  buffer.dir <- paste0(project.name, "/buffer/")
-  if (!dir.exists(buffer.dir)) dir.create(buffer.dir, recursive = TRUE, showWarnings = FALSE)
-  
-  raster.dir <- paste0(project.name, "/raw/")
+
+  raster.dir <- paste0(project.name)
   if (!dir.exists(raster.dir)) dir.create(raster.dir, recursive = TRUE, showWarnings = FALSE)
   
   # browser()
-  
-  avail_code = sub(".tif","", list.files(folder.iucn))
-  checklist.sub <- 
-    checklist %>% 
-    filter(Code %in% avail_code)
-  
-  listcode <- checklist.sub$Code
-  names(listcode) <- checklist.sub$SpeciesName
-  listname.iucn <- checklist.sub$NAME_IUCN
-  names(listname.iucn) <- checklist.sub$SpeciesName
-  listclass <- checklist.sub$Class
-  names(listclass) <- checklist.sub$SpeciesName
+  listcode <- checklist$Code
+  names(listcode) <- checklist$SpeciesName
+  listclass <- checklist$Class
+  names(listclass) <- checklist$SpeciesName
   # init log
   logfile_failed <- "bufferIUCN_failed.log"
   path_logfile_failed <- paste0(project.name,"/log/",logfile_failed)
@@ -81,91 +57,35 @@ rasterize_iucn <- function(checklist,
   path_logfile_success <- paste0(project.name,"/log/",logfile_success)
   if (file.exists(path_logfile_success)) file.remove(path_logfile_success)
   
-  # browser()
-  cli_progress_step("Load IUCN polygons")
-  list.iucn.vect <- lapply(list.iucn, vect)
-  list.iucn.df <- lapply(list.iucn.vect, as.data.frame)
-  # this.species = names(listcode)[1]
-  # this.species = names(listcode)[456]
-  # this.species = names(listcode)[510]
-  
   cli_progress_step("Running Species Loop")
   # species loop ------------------------------------------------------------
   
   foreach(this.species = names(listcode)) %dopar% {
     # browser()
+    this.class <- listclass[this.species]
     cli_progress_step(this.species)
-    this.name.iucn <- listname.iucn[[this.species]]
-    this.code <- listcode[[this.species]]
-    this.class <- listclass[[this.species]]
-    this.buffer <- buffer.config[[species.buffer[[this.species]]]]*1000
-    
-    # Change names of species whose name in .dbf files is given by SpeciesName
-    if (this.name.iucn == "Lyciasalamandra billae ssp. billae") {
-      this.name.iucn <-  "Lyciasalamandra billae"
-    }
-    if (this.name.iucn == "Vulpes lagopus") {
-      this.name.iucn <-  "Alopex lagopus"
-    }
-    if (grepl("Curruca", this.name.iucn)) {
-      this.name.iucn  <-  sub("Curruca", "Sylvia", this.name.iucn)
-    }
-    
-    if (this.class != "Aves") {
-      this.layer <- paste0(this.class,"_New")
-      this.index <- find_iucn_index(list.iucn.df[[this.layer]],
-                                    this.species,
-                                    species_col = "sci_name",
-                                    capitalized = FALSE)
-      # If no new IUCN range available get the old one
-      if (length(this.index) == 0) {
-        this.layer <- paste0(this.class,"_Old")
-        this.index <- find_iucn_index(list.iucn.df[[this.layer]],
-                                      this.species,
-                                      species_col = "BINOMIAL",
-                                      capitalized = TRUE)
-      }
-    } else {
-      this.layer <- paste0(this.class)
-      this.index <- find_iucn_index(list.iucn.df[[this.layer]],
-                                    this.species,
-                                    species_col = "SCINAME",
-                                    capitalized = TRUE)
-    }
-    
-    if (length(this.layer) == 0) {
+    this.code <- listcode[this.species]
+      this.iucn <-
+        locate_iucn_distribution(species.code = this.code,
+                                 folder.iucn = folder.iucn,
+                                 filetype = ".shp")
+
+    if (is.null(this.iucn)) {
       .write_logfile(
         out.log = paste0(this.species, " (", this.class,
-                         ") failed. Reason: no IUCN range found"),
+                         ") failed. Reason: Could not retrieve IUCN information"),
         logfile = logfile_failed,
         project.name = project.name,
         open = "a",
         silent = TRUE)
-    }
-    
-    this.poly <- list.iucn.vect[[this.layer]][this.index]
-    
-    # project and aggregate ----------------------------------------------------
-    project.try <- try({
-      this.poly.proj <- project(this.poly, data.mask)
-      if (length(this.index) > 1) {
-        this.poly <- aggregate(this.poly)
-      }
-    })
-    if (inherits(project.try, "try-error")) {
-      .write_logfile(
-        out.log = paste0(this.species, " (", this.class,
-                         ") failed. Project or aggregate failed"),
-        logfile = logfile_failed,
-        project.name = project.name,
-        open = "a",
-        silent = TRUE)
-      return(NULL)
-    }
-    # rasterize ----------------------------------------------------
-    if (do.raster) {
+
+      cli_process_failed()
+    }  else {
+      # get IUCN distribution
+      this.poly <- vect(this.iucn)
+      
       raster.try <- try({
-        this.rast <- rasterize(this.poly.proj, data.mask, background = 0)
+        this.rast <- rasterize(this.poly, data.mask, background = 0)
         this.rast <- mask(this.rast, mask = data.mask, maskvalue = NA,
                           filename = paste0(raster.dir, this.code, ".tif"),
                           overwrite = TRUE)
@@ -179,36 +99,20 @@ rasterize_iucn <- function(checklist,
           project.name = project.name,
           open = "a",
           silent = TRUE)
+        cli_process_failed()
         return(NULL)
+      } else {
+        cli_progress_done()
+        
       }
-    }
-    # buffer ----------------------------------------------------
-    if (do.buffer) {
-      buffer.try <- try({
-        this.poly.buffered <- buffer(this.poly.proj, width = this.buffer)
-        this.rast <- rasterize(this.poly.buffered, data.mask, background = 0)
-        this.rast <- mask(this.rast, mask = data.mask, maskvalue = NA,
-                          filename = paste0(buffer.dir, this.code, ".tif"),
-                          overwrite = TRUE)
-      })
-      if (inherits(buffer.try, "try-error")) {
-        .write_logfile(
-          out.log = paste0(this.species, " (", this.class,
-                           ") failed. Buffer failed"),
-          logfile = logfile_failed,
-          project.name = project.name,
-          open = "a",
-          silent = TRUE)
-        return(NULL)
-      }
-    }
-    cli_progress_done()
+    
     .write_logfile(
       out.log = this.species,
       logfile = logfile_success,
       project.name = project.name,
       open = "a",
       silent = TRUE)
+    }
     NULL
   }
   if (has.cluster) doParallel::stopImplicitCluster()
@@ -219,27 +123,13 @@ rasterize_iucn <- function(checklist,
 
 .rasterize_iucn.check.args <- function(checklist, 
                                        folder.iucn,
-                                       list.iucn,
                                        data.mask,
                                        project.name,
-                                       buffer.config,
-                                       nb.cpu,
-                                       do.raster,
-                                       do.buffer){
+                                       nb.cpu){
   
   
   #### checklist -------------------------------------------------------------
   .check_checklist(checklist)
-  
-  #### list.iucn -----------------------------------------------------------
-  name.iucn.tot <- c("Amphibia_Old", "Amphibia_New", 
-                     "Mammalia_Old", "Mammalia_New",
-                     "Reptilia_Old", "Reptilia_New", 
-                     "Aves") 
-  if (!all(name.iucn.tot %in% names(list.iucn)) ||
-      !all(names(list.iucn) %in% name.iucn.tot)) {
-    stop("list.iucn must have name corresponding to ", paste0(name.iucn.tot, collapse = ", "),".")
-  }
   
   #### data.mask -------------------------------------------------------------
   .fun_testIfInherits(data.mask, "SpatRaster")
@@ -254,29 +144,12 @@ rasterize_iucn <- function(checklist,
   
   #### nb.cpu -------------------------------------------------------  
   .fun_testIfPosInt(nb.cpu)
-  #### do.raster -------------------------------------------------------  
-  stopifnot(is.logical(do.raster))
-  #### do.buffer -------------------------------------------------------  
-  stopifnot(is.logical(do.buffer))
-  
-  #### buffer.config -----------------------------------------------------------
-  if (do.buffer && missing(buffer.config)) {
-    stop("Please provide argument buffer.config, a named list with the distance used to buffer IUCN range, by taxonomic group")
-  } else {
-    .fun_testIfPosNum(unlist(buffer.config))
-    species.buffer <- get_species_buffer(buffer.config, checklist)
-  }
-  
+
   return(list(
     "checklist" = checklist, 
-    "list.iucn" = list.iucn,
     "data.mask" = data.mask,
     "project.name" = project.name,
-    "buffer.config" = buffer.config,
-    "species.buffer" = species.buffer,
-    "nb.cpu" = nb.cpu,
-    "do.raster" = do.raster,
-    "do.buffer" = do.buffer
+    "nb.cpu" = nb.cpu
   ))
 }
 
@@ -330,7 +203,7 @@ buffer_iucn <- function(checklist,
   
   has.cluster <- .register_cluster(nb.cpu = nb.cpu)
   
-  buffer.dir <- paste0(project.name, "/buffer/")
+  buffer.dir <- paste0(project.name)
   if (!dir.exists(buffer.dir)) dir.create(buffer.dir, recursive = TRUE, showWarnings = FALSE)
   
   avail_code = sub(".tif","", list.files(folder.iucn.raster))
@@ -403,6 +276,7 @@ buffer_iucn <- function(checklist,
         this.rast <- mask(this.rast, mask = data.mask, maskvalue = NA,
                           filename = this.out.file,
                           overwrite = TRUE)
+        rm(this.poly, this.poly.buffered, this.rast); gc();
       })
       if (inherits(buffer.try, "try-error")) {
         cli_process_failed()
